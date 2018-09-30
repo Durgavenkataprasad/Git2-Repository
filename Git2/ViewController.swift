588f5bf015b22e3b361bf2fc4f3b929f85bb6d4a
Your Name <you@example.com>
2018-09-30 11:46:14 +0530

    edited

--- Git2/ViewController.swift
+++ Git2/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Git project imported")
+        
+        print("edited")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
